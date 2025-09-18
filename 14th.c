303bd5fc28bf1cbb70c8a6c334737b4b0fb6b8c5
Tarang1411 <tarangraghuwanshi96@gmail.com>
2025-09-18 15:44:00 +0530

    lect

---
+++ 14th.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, -3 ,4, -5};
+    int maxsum=0;
+
+    int cursum = 0;
+    for (int i = 0; i < 5; i++) {
+        cursum = cursum + arr[i];
+        if (cursum > maxsum) {
+            maxsum = cursum;
+        }
+        if (cursum < 0) {
+            cursum = 0;
+        }
+    }
+    printf("%d", maxsum);
+
+    return 0;
+}
